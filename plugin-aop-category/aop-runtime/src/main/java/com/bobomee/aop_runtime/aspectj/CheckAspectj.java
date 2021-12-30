f28ef6a10956a8bda13ee1e5ef07b9e12a8d0f4a
wangbo <wangbo02@noboauto.com>
2021-12-30 19:57:58 +0800

    update

--- plugin-aop-category/aop-runtime/src/main/java/com/bobomee/aop_runtime/aspectj/CheckAspectj.java
+++ plugin-aop-category/aop-runtime/src/main/java/com/bobomee/aop_runtime/aspectj/CheckAspectj.java
@@ -2,6 +2,7 @@ package com.bobomee.aop_runtime.aspectj;
 
 import com.bobomee.aop_annotation.Check;
 import com.bobomee.aop_runtime.CheckerHolder;
+import com.bobomee.aop_runtime.checker.Checker;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -36,14 +37,13 @@ public class CheckAspectj {
     @Around("(method() || constructor()) && @annotation(check)")
     public Object aroundJoinPoint(ProceedingJoinPoint joinPoint, Check check) throws Throwable {
         Object[] args = joinPoint.getArgs();
-        if (CheckerHolder.getChecker() == null) {
-            return joinPoint.proceed(args);
-        }
         boolean result = check(check.value(), joinPoint);
         return result ? null : joinPoint.proceed(args);
     }
 
     private boolean check(String[] value, ProceedingJoinPoint joinPoint) {
-        return CheckerHolder.getChecker().check(value, joinPoint);
+        Checker checker = CheckerHolder.getChecker();
+        if (null == checker)return false;
+        else return checker.check(value, joinPoint);
     }
 }
